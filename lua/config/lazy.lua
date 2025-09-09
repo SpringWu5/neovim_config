81bb9902f6b4e1383cb5195b63f952bb8f43ef39
spring5 <spring5@sjtu.edu.cn>
2025-09-09 15:09:26 +0800

    Add patch to disable filters for old git version

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,7 +1,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local out = vim.fn.system({ "git", "clone", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -15,6 +15,14 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  -- =======================================================--
+  --  牛马服务器兼容补丁
+  -- =======================================================--
+  git = {
+    -- 禁用 --filter=blob:none 功能，兼容老旧的 Git 版本
+    filter = false,
+  },
+  -- =======================================================--
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "everforest" } },
